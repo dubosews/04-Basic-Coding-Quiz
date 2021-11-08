f6c3abc1f0a7f091673299b32ea087306cae1b0d
Wesley DuBose <wsd10205@gmail.com>
2021-11-07 21:29:34 -0500

    Final Commit
    Final without high score.

---
+++ assets/script/questions.js
@@ -0,0 +1,57 @@
+// Creating an Array to pass questions, question number, question answer and question options
+let questions = [
+        {
+        numb:1,
+        question: "Inside which HTML element do we insert Javascript?",
+        answer: "script",
+        options: [
+            "script",
+            "javascript",
+            "js",
+            "scripting",
+        ]
+    },
+        {
+        numb:2,
+        question: "What basic command is used in order to display text in the console?",
+        answer: "console.log('.'+ varAny)",
+        options: [
+            "console.log('.' + varAny)",
+            "document.get(print.console)",
+            "console.print(documentVar)",
+            "print where='console'",
+        ]
+    },
+    {
+        numb:3,
+        question: "True or False: Javascript can only be added to the index.html file.",
+        answer: "False",
+        options: [
+            "True",
+            "False",
+        ]
+    },
+    {
+        numb:4,
+        question: "How do you generate a random number in Javascript?",
+        answer: "Math.random()",
+        options: [
+            "num.random()",
+            "Math.random()",
+            "var= random.number",
+            "generate.random(num)",
+        ]
+    },
+    {
+        numb:5,
+        question: "How do you add a single line comment in Javascript?",
+        answer: "// comment text here",
+        options: [
+            "/* comment text here */",
+            "// comment text here",
+            "(( comment text here))",
+            "/comment text here/",
+        ]
+    },
+    
+]
